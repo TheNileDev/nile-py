fa03dc8e7868c5261f64f3919cd08e971982b824
Julian Berman <Julian@GrayVines.com>
2022-10-11 17:30:29 +0000

    And add a way to regenerate the client.

--- noxfile.py
+++ noxfile.py
@@ -1,10 +1,12 @@
 from pathlib import Path
+
 import nox
 
 OPENAPI_URL = "https://prod.thenile.dev/openapi.yaml"
 
 ROOT = Path(__file__).parent
 GENERATE_REQUIREMENTS = ROOT / "openapi-generator-requirements"
+GENERATE_CONFIG = ROOT / "openapi-generator-config.yml"
 
 nox.options.sessions = []
 
@@ -22,3 +24,18 @@ def session(default=True, **kwargs):
 def update_openapi_requirements(session):
     session.install("pip-tools")
     session.run("pip-compile", "-U", "-r", f"{GENERATE_REQUIREMENTS}.in")
+
+
+@session(default=False)
+def regenerate(session):
+    session.install("-r", f"{GENERATE_REQUIREMENTS}.txt")
+    # See openapi-generators/openapi-python-client#684
+    with session.chdir(ROOT.parent):
+        session.run(
+            "openapi-python-client",
+            "update",
+            "--url",
+            OPENAPI_URL,
+            "--config",
+            str(GENERATE_CONFIG),  # str() until wntrblm/nox#649 is released
+        )
