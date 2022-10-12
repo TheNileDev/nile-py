1a258bbe161324ab0e84b0442be5c29cd73fee1a
Julian Berman <Julian@GrayVines.com>
2022-10-12 13:32:28 +0000

    Packaging setup (via hatchling) and a trivial test.
    Just checks that the package is importable.

--- noxfile.py
+++ noxfile.py
@@ -20,6 +20,27 @@ def session(default=True, **kwargs):
     return _session
 
 
+@session(python=["3.7", "3.8", "3.9", "3.10", "pypy3"])
+def tests(session):
+    session.install("pytest")
+    session.run("pytest")
+
+
+@session(tags=["build"])
+def build(session):
+    session.install("build")
+    tmpdir = session.create_tmp()
+    session.run("python", "-m", "build", str(ROOT), "--outdir", tmpdir)
+
+
+@session(tags=["style"])
+def readme(session):
+    session.install("build", "twine")
+    tmpdir = session.create_tmp()
+    session.run("python", "-m", "build", str(ROOT), "--outdir", tmpdir)
+    session.run("python", "-m", "twine", "check", tmpdir + "/*")
+
+
 @session(default=False)
 def update_openapi_requirements(session):
     session.install("pip-tools")
